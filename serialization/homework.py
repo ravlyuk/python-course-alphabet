70a2754ac351e18c8896f79abd8682407ec452cd
ravlyuk <vivexpro@gmail.com>
2019-05-15 20:58:30 +0300

    Update HW_5

--- serialization/homework.py
+++ serialization/homework.py
@@ -67,7 +67,6 @@ Advanced
     Колекціонерів можна порівнювати за ціною всіх їх автомобілів.
 """
 
-
 import random
 import uuid
 from constants import *
@@ -81,10 +80,6 @@ import configparser
 import io
 
 
-
-
-
-
 class Cesar:
 
     def __init__(self, name, garages=0):
@@ -92,9 +87,11 @@ class Cesar:
         self.garages = []
         self.register_id = uuid.uuid4()
 
-    def __str__(self): return f'\nname: {self.name}\ngarages: {self.garages}\nregister_id: {self.register_id}'
+    def __str__(self):
+        return f'\nname: {self.name}\ngarages: {self.garages}\nregister_id: {self.register_id}'
 
-    def __repr__(self): return f'{vars(self)}'
+    def __repr__(self):
+        return f'{vars(self)}'
 
     def add_garage(self, the_garage):
         if not the_garage.owner:
@@ -103,9 +100,11 @@ class Cesar:
         else:
             print('This garage already belongs to another collector.')
 
-    def garages_count(self): return len(self.garages)
+    def garages_count(self):
+        return len(self.garages)
 
-    def cars_count(self): return sum([len(garage.get('cars')) for garage in self.garages])
+    def cars_count(self):
+        return sum([len(garage.get('cars')) for garage in self.garages])
 
     def hit_hat(self):
         # return sum([int(garage.get('hit_hat_var')) for garage in self.garages])
@@ -127,8 +126,6 @@ class Cesar:
             else:
                 print("In this garage there are no empty seats.")
 
-
-
     def __lt__(self, other):
         return self.hit_hat() < other.hit_hat()
 
@@ -148,9 +145,6 @@ class Cesar:
         return self.hit_hat() >= other.hit_hat()
 
 
-
-
-
 class Car:
 
     def __init__(self, price, mileage: float, producer, type):
@@ -163,8 +157,6 @@ class Car:
     def __str__(self):
         return f'\nprice: {self.price}\ntype: {self.type}\nproducer: {self.producer}\nnumber: {self.number}\n' \
             f'mileage: {self.mileage}'
-
-
 
     def __repr__(self): return f'{vars(self)}'
 
@@ -183,38 +175,37 @@ class Car:
     def __ge__(self, other): return self.price >= other.price
 
 
-
 class Garage:
 
-
-    def __init__(self, town, places: int,  number=None, owner=None):
+    def __init__(self, town, places: int, number=None, owner=None):
         self.town = town
         self.places = places
         self.owner = owner
         self.number = uuid.uuid4()
         self.cars = []
 
+    def __str__(self):
+        return f'\ntown: {self.town}\nplaces: {self.places}\nowner: {self.owner}\ncars: {self.cars}'
 
-
-    def __str__(self): return f'\ntown: {self.town}\nplaces: {self.places}\nowner: {self.owner}\ncars: {self.cars}'
-
-
-    def __repr__(self):return f'{vars(self)}'
+    def __repr__(self):
+        return f'{vars(self)}'
 
     def add_car_into_garage(self, car):
         if self.places:
             self.cars.append(car)
-        else: print("The garage is full!")
+        else:
+            print("The garage is full!")
         self.places -= 1
 
     def remove(self, car):
-        if car in self.cars: self.cars.remove(car)
-        else: print("This car is not in the garage!")
+        if car in self.cars:
+            self.cars.remove(car)
+        else:
+            print("This car is not in the garage!")
         self.places += 1
 
     def hit_hat(self):
         return sum([car.price for car in self.cars])
-
 
 
 class JsonConverter(json.JSONEncoder):
@@ -250,7 +241,6 @@ class JsonConverter(json.JSONEncoder):
         return json.JSONEncoder.default(self, object)
 
 
-
 # === Installation objects ===
 
 auto1 = Car(5000, 2, random.choice(CARS_PRODUCER), random.choice(CARS_TYPES))
@@ -264,8 +254,6 @@ cesar3 = Cesar('Vin Diesel')
 garage1 = Garage(random.choice(TOWNS), 2)
 garage2 = Garage(random.choice(TOWNS), 3)
 garage3 = Garage(random.choice(TOWNS), 5)
-
-
 
 # === Add auto to garage ===
 
@@ -281,15 +269,11 @@ print(colored(f'\n\nAdd auto3 to garage 3', 'blue'))
 garage3.add_car_into_garage(auto3)
 print(garage3)
 
-
-
 # === Remove car to garage 1 ===
 
 print(colored(f'\n\nRemove auto2 in garage 1', 'blue'))
 garage1.remove(auto2)
 print(garage1)
-
-
 
 # === The sum price of all auto in the garage 1 ===
 
@@ -297,15 +281,11 @@ print(colored(f'\n\nThe sum price of all cars in the garage 1\n', 'blue'))
 result_hit_hat = garage1.hit_hat()
 print(colored('SUM : ', 'green'), result_hit_hat)
 
-
-
 # === Add garage 1 to cesar 1 ===
 
 print(colored(f'\n\nAdd "garage 1" to "cesar 1"\n', 'blue'))
 cesar1.add_garage(garage1)
 print(cesar1.garages)
-
-
 
 # === Add garage 3 to cesar 3 ===
 
@@ -313,21 +293,15 @@ print(colored(f'\n\nAdd "garage 3" to "cesar 3"\n', 'blue'))
 cesar3.add_garage(garage3)
 print(cesar3.garages)
 
-
-
 # === Garage Count in "cesar 1" ===
 
 print(colored(f'\n\nGarage Count in "cesar 1"\n', 'blue'))
 print(colored('Count : ', 'green'), cesar1.garages_count())
 
-
-
 # === Garage Count in "cesar 2" ===
 
 print(colored(f'\n\nGarage Count in "cesar 2"\n', 'blue'))
 print(colored('Count : ', 'green'), cesar2.garages_count())
-
-
 
 # === Car change number ===
 
@@ -336,35 +310,29 @@ print(colored('Before : ', 'green'), auto3.number)
 auto3.change_car_number()
 print(colored('After  : ', 'green'), auto3.number)
 
-
-
 # === Car comparison ===
 
 print(colored(f'\n\nCar comparison\n', 'blue'))
 
-print(colored('<  : ', 'green'), auto1 <  auto2)
+print(colored('<  : ', 'green'), auto1 < auto2)
 print(colored('<= : ', 'green'), auto1 <= auto2)
 print(colored('== : ', 'green'), auto1 == auto2)
 print(colored('!= : ', 'green'), auto1 != auto2)
-print(colored('>  : ', 'green'), auto1 >  auto2)
+print(colored('>  : ', 'green'), auto1 > auto2)
 print(colored('>= : ', 'green'), auto1 >= auto2)
-
-
 
 # === Cesar comparison ===
 
 print(colored(f'\n\nCesar comparison\n', 'blue'))
-print(colored('<  : ', 'green'), cesar1 <  cesar3)
+print(colored('<  : ', 'green'), cesar1 < cesar3)
 print(colored('<= : ', 'green'), cesar1 <= cesar3)
 print(colored('== : ', 'green'), cesar1 == cesar3)
 print(colored('!= : ', 'green'), cesar1 != cesar3)
-print(colored('>  : ', 'green'), cesar1 >  cesar3)
+print(colored('>  : ', 'green'), cesar1 > cesar3)
 print(colored('>= : ', 'green'), cesar1 >= cesar3)
 
 
-
 # -------HOMEWORK SERELISATION
-
 
 
 def car_deserial(obj):
@@ -396,16 +364,12 @@ def cesar_deserial(obj):
 
 
 def json_hook(obj):
-    print('\n------')
-    print(obj)
-    print('------\n')
     if obj['__class__'] == "Car":
         return car_deserial(obj)
     elif obj['__class__'] == "Garage":
         return garage_deserial(obj)
     elif obj['__class__'] == "Cesar":
         return cesar_deserial(obj)
-
 
 
 # JSON SERIALIZATION TO STRING
@@ -425,61 +389,43 @@ print(json_serialized_garage)
 print(colored("\n\ncesar 1 - JSON SERIALIZATION TO FILE\n", 'blue'))
 print(json_serialized_cesar)
 
-
-
 # JSON SERIALIZATION TO FILE
 
 # print(colored("\n\n=========  JSON SERIALIZATION TO FILE ========= \n", 'blue'))
-#
-
-#
-#
-#
-# with open('auto1_serialization.json', 'w') as file:
-#     json.dump(auto1, file, cls=JsonConverter, indent=4)
-#     print('auto1_serialization.json - COMPLETE\n')
-#
-#
-#
-# with open('garage1_serialization.json', 'w') as file:
-#     json.dump(garage1, file, cls=JsonConverter, indent=4)
-#     print('garage1_serialization.json - COMPLETE\n')
-#
-#
-#
-# with open('cesar1_serialization.json', 'w') as file:
-#     json.dump(cesar1, file, cls=JsonConverter, indent=4)
-#     print('cesar1_serialization.json - COMPLETE\n')
-#
-# #
-#
-# # JSON DESERIALIZATION WITHOUT FILE
-#
-# print(colored("\n\nJSON DESERIALIZATION WITHOUT FILE\n", 'blue'))
-#
-# print("\n\nStarting auto1_serialization")
-# with open('auto1_serialization.json', 'r') as file:
-#     auto1_deserialization = json.load(file, object_hook=json_hook)
-# print(colored('\n\nauto1 deserialization:', 'green'))
-# print(auto1_deserialization)
 
 
+with open('auto1_serialization.json', 'w') as file:
+    json.dump(auto1, file, cls=JsonConverter, indent=4)
+    print('auto1_serialization.json - COMPLETE\n')
 
+with open('garage1_serialization.json', 'w') as file:
+    json.dump(garage1, file, cls=JsonConverter, indent=4)
+    print('garage1_serialization.json - COMPLETE\n')
 
+with open('cesar1_serialization.json', 'w') as file:
+    json.dump(cesar1, file, cls=JsonConverter, indent=4)
+    print('cesar1_serialization.json - COMPLETE\n')
 
-
-
-# print("\n\nStarting garage1_serialization")
-# with open('garage1_serialization.json', 'r') as file:
-#     garage1_deserialization = json.load(file, object_hook=json_hook)
-# print(colored('\n\ngarage1 deserialization:', 'green'))
-# print(garage1_deserialization)
 #
-#
-# print("\n\nStarting cesar1_serialization")
-# with open('cesar1_serialization.json', 'r') as file:
-#     cesar1_deserialization = json.load(file, object_hook=json_hook)
-# print(colored('\n\ncesar1 deserialization:', 'green'))
-# print(cesar1_deserialization)
 
+# JSON DESERIALIZATION WITHOUT FILE
 
+print(colored("\n\nJSON DESERIALIZATION WITHOUT FILE\n", 'blue'))
+
+print("\n\nStarting auto1_serialization")
+with open('auto1_serialization.json', 'r') as file:
+    auto1_deserialization = json.load(file, object_hook=json_hook)
+print(colored('\n\nauto1 deserialization:', 'green'))
+print(auto1_deserialization)
+
+print("\n\nStarting garage1_serialization")
+with open('garage1_serialization.json', 'r') as file:
+    garage1_deserialization = json.load(file, object_hook=json_hook)
+print(colored('\n\ngarage1 deserialization:', 'green'))
+print(garage1_deserialization)
+
+print("\n\nStarting cesar1_serialization")
+with open('cesar1_serialization.json', 'r') as file:
+    cesar1_deserialization = json.load(file, object_hook=json_hook)
+print(colored('\n\ncesar1 deserialization:', 'green'))
+print(cesar1_deserialization)
