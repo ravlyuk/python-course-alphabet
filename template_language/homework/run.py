b18093e4e80c7f81994d039c1d20abfcacee73eb
ravlyuk <vivexpro@gmail.com>
2019-05-26 19:17:29 +0300

    HW template

--- template_language/homework/run.py
+++ template_language/homework/run.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 with open('movies.json') as f:
     MOVIES = json.load(f)
 
@@ -22,9 +21,10 @@ def movies_page():
 @app.route('/<title>')
 def movie_page(title):
     for i, movie in enumerate(MOVIES):
-        if MOVIES[i].get('title') == title:
-            return render_template('movie.html', title=title, movie=MOVIES[i])
-    return render_template('movies.html', title='Movies list', movies=MOVIES)
+        for i, movie in enumerate(MOVIES):
+            if MOVIES[i].get('title') == title:
+                return render_template('movie.html', title=title, movie=MOVIES[i])
+        return render_template('movies.html', title='Movies list', movies=MOVIES)
 
 
 if __name__ == '__main__':
