4c7a23051018fe38579e4f4aefe2068c9e4bd50b
ravlyuk <vivexpro@gmail.com>
2019-06-04 18:58:23 +0300

    fix routes and del unused py files

--- flask-lesson_1/homework/fruits/routes.py
+++ flask-lesson_1/homework/fruits/routes.py
@@ -6,13 +6,14 @@ fruits_list = ['Apple', 'Banana', 'Lemon', 'Kiwi', 'Orange']
 
 
 @fruits.route('/fruits')
-@fruits.route('/fruits/<string:value>', methods=['GET', 'POST', 'DELETE'])
-def fruit_manager(value=None):
+def fruit_manager():
+    return render_template('fruits.html', title='Fruits', fruits_list=fruits_list)
+
+@fruits.route('/fruits/<string:value>', methods=['POST', 'DELETE'])
+def fruit_route_manager(value=None):
     if request.method == "POST":
         fruits_list.append(value)
         return f"\"{value}\" added!"
     elif request.method == "DELETE":
         fruits_list.remove(value)
         return f"\"{value}\" deleted!"
-    elif request.method == "GET":
-        return render_template('fruits.html', title='Fruits', fruits_list=fruits_list)
--- flask-lesson_1/homework/vegetables/routes.py
+++ flask-lesson_1/homework/vegetables/routes.py
@@ -5,14 +5,17 @@ vegetables = Blueprint('vegetables', __name__, )
 vegetables_list = ['Tomato', 'Potato', 'Cucumber', 'Pepper', 'Сabbage']
 
 
+
+
 @vegetables.route("/vegetables")
-@vegetables.route('/vegetables/<string:value>', methods=['GET', 'POST', 'DELETE'])
-def vegetables_manager(value=None):
+def vegetables_manager():
+    return render_template('vegetables.html', title='Vegetables', vegetables_list=vegetables_list)
+
+@vegetables.route('/vegetables/<string:value>', methods=['POST', 'DELETE'])
+def vegetables_route_manager(value=None):
     if request.method == "POST":
         vegetables_list.append(value)
         return f"\"{value}\" added!"
     elif request.method == "DELETE":
         vegetables_list.remove(value)
         return f"\"{value}\" deleted!"
-    elif request.method == "GET":
-        return render_template('vegetables.html', title='Vegetables', vegetables_list=vegetables_list)
